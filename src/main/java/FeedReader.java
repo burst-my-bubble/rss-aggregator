4c0c179abd58448db38c6e6a1ca09512634aac27
Hugo Mayo <hzm17@point02.doc.ic.ac.uk>
2019-05-22 11:57:25 +0100

    Refactors code to get uri into a separate method

--- src/main/java/FeedReader.java
+++ src/main/java/FeedReader.java
@@ -1,5 +1,8 @@
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
@@ -14,33 +17,42 @@ import com.rometools.rome.io.XmlReader;
      */
     public class FeedReader {
 
+        private static final List<String> newsSources = List.of("http://feeds.bbci.co.uk/news/rss.xml",
+                "http://rss.cnn.com/rss/edition.rss",
+                "https://www.dailymail.co.uk/articles.rss",
+                "https://www.buzzfeed.com/index.xml",
+                "https://www.theguardian.com/uk/rss",
+                "https://9to5mac.com/feed/",
+                "https://www.thesun.co.uk/feed/",
+                "https://www.telegraph.co.uk/rss.xml",
+                "https://www.huffingtonpost.co.uk/feeds/index.xml",
+                " http://www.pinknews.co.uk/feed/");
+
+        private static List<String> getUri(String url) {
+            List<String> articles = new ArrayList<>();
+            try {
+                URL feedUrl = new URL(url);
+
+                SyndFeedInput input = new SyndFeedInput();
+                SyndFeed feed = input.build(new XmlReader(feedUrl));
+
+                for (SyndEntry entry : feed.getEntries()) {
+                    articles.add(entry.getUri());
+                }
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("ERROR: "+ex.getMessage());
+            }
+            return articles;
+        }
+
         public static void main(String[] args) {
-            boolean ok = false;
-            if (args.length==1) {
-                try {
-                    URL feedUrl = new URL(args[0]);
-
-                    SyndFeedInput input = new SyndFeedInput();
-                    SyndFeed feed = input.build(new XmlReader(feedUrl));
-
-
-                    ok = true;
-                    for (SyndEntry entry : feed.getEntries()) {
-                        System.out.println(entry.getUri());
-                    }
-                }
-                catch (Exception ex) {
-                    ex.printStackTrace();
-                    System.out.println("ERROR: "+ex.getMessage());
-                }
+            HashMap<String, List<String>> articles = new HashMap<>();
+            for (String sourceUrl : newsSources) {
+                articles.put(sourceUrl, getUri(sourceUrl));
             }
-
-            if (!ok) {
-                System.out.println();
-                System.out.println("FeedReader reads and prints any RSS/Atom feed type.");
-                System.out.println("The first parameter must be the URL of the feed to read.");
-                System.out.println();
-            }
+            System.out.println(articles.get("https://www.thesun.co.uk/feed/"));
         }
 
     }
