7535980e3192f9c89277f57f0ba682c9b0f195e9
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2019-05-28 15:32:56 +0100

    Adds test stage to CI pipeline

---
+++ test/main/java/ControllerTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+  @Test
+  public void onePlusOneShouldBeTwo() {
+    assertEquals(1 + 1, 2);
+  }
+}
