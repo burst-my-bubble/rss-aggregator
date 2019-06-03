09219eeff16d5e3e037e42b2edd241859c99dab8
jackmorrison <jsm1317@ic.ac.uk>
2019-06-03 18:05:25 +0100

    Changes limit to 5 articles per feed

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -219,7 +219,7 @@ public class Controller {
       List<Article> articles = reader.getArticles(feed.getFirst());
       List<Article> newArticles = articles.stream()
           .filter(a -> !storage.urlExists(a.getUrl()))
-          .limit(10)
+          .limit(5)
           .collect(Collectors.toList());
 
       newArticles.forEach(a -> a.setFeed(feed));
