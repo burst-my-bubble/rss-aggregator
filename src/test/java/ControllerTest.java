d892a2996c9039ba361d7b92b41fbbf42f2de783
Hugo Mayo <hzm17@ic.ac.uk>
2019-06-05 15:38:01 +0100

    [jar17] Corrects test for getting plain text now using url

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -16,16 +16,9 @@ class ControllerTest {
 
   @Test
   public void getPlainTextExtractsPlainTextFromHTML() throws BoilerpipeProcessingException, IOException {
-    final String htmlText ="<blockquote class=\"twitter-tweet\" data-conversation=\"none\" " +
-      "data-lang=\"en\"><p lang=\"en\" dir=\"ltr\">3. I am and always will be Labour.  But hard " +
-      "not to point out difference in the way anti-Semitism cases have been handled.</p>&mdash;" +
-      " Alastair PEOPLE’S VOTE Campbell (@campbellclaret) " +
-      "<a href=\"https://twitter.com/campbellclaret/status/1133332264589373445?ref_src=twsrc%5Etfw\">" +
-      "May 28, 2019</a></blockquote>";
-    final String expectedOutput = "3. I am and always will be Labour.  But hard not to point out " + 
-      "difference in the way anti-Semitism cases have been handled.\n";
-
-    assertEquals(expectedOutput, Controller.getPlainText(htmlText));
+    final String url = "https://example.com";
+    String htmlText = Controller.getPlainText(url);
+    assert(htmlText.contains("This domain is established to be used for illustra"));
   }
 
   @Test
